3754d07031b74c2555936894e6d3ae0b4193543e
carcablop <carolina.cl19@gmail.com>
2022-12-26 16:19:05 -0500

    Update service.py to String format

--- src/service.py
+++ src/service.py
@@ -88,7 +88,7 @@ class Model(object):
             h = next(reader)
             R = []
             for r in reader:
-                R += [{"outcome": [Float(x) for x in r]}] # <-- EDIT: Modify according to type of output (Float, String...)
+                R += [{"outcome": [String(x) for x in r]}] # <-- EDIT: Modify according to type of output (Float, String...)
         meta = {
             "outcome": h
         }
