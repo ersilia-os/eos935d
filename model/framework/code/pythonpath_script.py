9ea70417a514023dda5f41ae103d72987589bc9e
HellenNamulinda <hellennamulinda@gmail.com>
2023-08-07 06:44:04 +0300

    Remove function for python path

--- model/framework/code/pythonpath_script.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def getPythonPath_env(name_env_model):
-    python_path= subprocess.check_output("which python", shell=True).strip()
-    index_env=python_path.decode('utf-8').find("envs/") + 5
-    python_path_envs_model= python_path.decode('utf-8')[0:index_env]
-    print ("path",python_path_envs_model)
-    
-    return python_path_envs_model + name_env_model + "/bin/python"
